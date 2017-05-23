d220c195a84889845ee7a334cf9a5f487ef8e71e
rlbagad <rahul.bagad@walchandsangli.ac.in>
2017-05-23 15:31:50 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pending Submission Notifier
+
+This python script notifies the admin of puzzlepedia about pending submissions.
+Tested on ubuntu 15.04
+Python 2.7
+Reference: http://www.geeksforgeeks.org/desktop-notifier-python/
---
+++ src/psn.py
@@ -0,0 +1,59 @@
+import time
+import notify2
+import cookielib
+import urllib
+import urllib2
+
+
+cj = cookielib.CookieJar()
+
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+urllib2.install_opener(opener)
+
+authentication_url = 'http://puzzlepedia.esy.es/adminlogin.php'
+
+payload = {
+  'lemail': 'rlbagad2@gmail.com',
+  'lpwd': 'asdf'
+  }
+
+data = urllib.urlencode(payload)
+
+req = urllib2.Request(authentication_url, data)
+
+resp = urllib2.urlopen(req)
+contents = resp.read()
+
+url=urllib2.urlopen("http://puzzlepedia.esy.es/evaluate.php")
+s = url.read()
+
+
+while 'submission' in s:
+	# path to notification window icon
+	ICON_PATH = "/home/rahul/Downloads/PSC/src/notify.png"
+	 
+	 
+	# initialise the d-bus connection
+	notify2.init("Pending Submission Notifier")
+	 
+	# create Notification object
+	n = notify2.Notification(None, icon = ICON_PATH)
+	 
+	# set urgency level
+	n.set_urgency(notify2.URGENCY_NORMAL)
+	 
+	# set timeout for a notification
+	n.set_timeout(10000)
+	 
+	# update notification data for Notification object
+	n.update("Puzzlepedia","Submission Checking Pending")
+	 
+	# show notification on screen
+	n.show()
+	 
+	# short delay between notifications
+	time.sleep(15)
+
+
+
